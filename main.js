c32549724a1ea4e27b5e025213615656a6040e05
Anshul M <anshmaske743@gmail.com>
2023-07-21 15:32:16 -0700

    Added playRound function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // alert("🪨 📄 ✂️");
 
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = ['rock', 'paper', 'scissors'];
 
 function pickOne(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -8,4 +8,45 @@ function pickOne(arr) {
 
 function getComputerChoice() {
     return pickOne(choices);
+}
+
+function playRound(comp, usr) {
+    if (usr.match(comp))
+    {
+        console.log("It's a tie!");
+        return 0;
+    }
+
+    if (usr.match('rock') && comp.match('paper'))
+    {
+        console.log("You Lose!");
+        return -1;
+    }
+    if (comp.match('rock') && usr.match('paper'))
+    {
+        console.log("You Win!");
+        return 1;
+    }
+
+    if (usr.match('rock') && comp.match('scissor'))
+    {
+        console.log("You Win!");
+        return 1;
+    }
+    if (comp.match('rock') && usr.match('scissor'))
+    {
+        console.log("You Lose!");
+        return -1;
+    }
+
+    if (usr.match('scissor') && comp.match('paper'))
+    {
+        console.log("You Win!");
+        return 1;
+    }
+    if (comp.match('scissor') && usr.match('paper'))
+    {
+        console.log("You Lose!");
+        return -1;
+    }
 }
